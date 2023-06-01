ae5667c69d423e1ace45058d4d946ba96f1e8fde
MartaBelmonte <martabelmonts5@gmail.com>
2023-06-01 19:58:47 +0200

    ticket3

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//variables
+// - careto (para hacerle click a él)
+// - crear cara con guiño en html y tenerla ocultada?
+
+//funciones
+//- cuando se haga click en el careto tiene que guiñar el ojo
+// - cuando se deje de hacer hover tiene que volver a careto normal
+
+//eventos
+//-llamar a la funcion cuando hagas click
